b745648720a96f6f3c095a000ca83e1351e780e9
KasiaOT <167691567+katarzynaOT@users.noreply.github.com>
2025-01-19 14:19:50 +0100

    Update kierownik_kas.c

--- kierownik_kas.c
+++ kierownik_kas.c
@@ -10,13 +10,16 @@
 #include <sys/msg.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
+#include <sys/stat.h>
 #include <ctype.h>
 #include <errno.h>
 #include <time.h>
 #include <pthread.h>
 #include <sys/prctl.h> //prctl
+#include <fcntl.h>
 
 
+#define MAX_TEXT 20 //ilosc znakow do zapisu do kolejki
 #define DZIEN 30 //czas trwania jednego dnia
 
 int N; //liczba czynnych kas
@@ -24,6 +27,12 @@ int K; //liczba klientow w sklepie
 key_t klucz = 21370; //ftok("pp", 127);
 
 int id_sem; 
+int id_kolejki;
+struct msg_buffer {
+	long mtype;
+	long ktype;
+	char mtext[MAX_TEXT];
+};
 
 static void sem_p(int nr) { //opuszczenie semafora
     struct sembuf bufor_sem;
@@ -60,80 +69,164 @@ void pisz_raport(const char *tresc) {
     fclose(file);
 }
 
-//otwieranie po nocy
+//otwieranie po nocy - wlacz semafory, wyczysc ksiege gosciS
 void otworz_sklep(void) {
     if (semctl(id_sem, 0, SETVAL, 30) == -1) { //semafor ochroniarz 1 (odpowiada za to, by sklep sie nie przepelnil)
         perror("blad ustawienia semafora 0");
         exit(EXIT_FAILURE);
     }
-    if (semctl(id_sem, 1, SETVAL, 1) == -1) { //manager sklepu (blokada raportu (otwarty)
+    if (semctl(id_sem, 1, SETVAL, 1) == -1) { //manager sklepu (blokada ksiegi_gosci - otwarty)
         perror("blad ustawienia semafora 1");
         exit(EXIT_FAILURE);
     }
-    if (semctl(id_sem, 2, SETVAL, 2) == -1) { //2 kasy otwarte
-        perror("blad ustawienia semafora 1");
+    if (semctl(id_sem, 2, SETVAL, 1) == -1) { //semafor ochroniarz 2 (blokada sklepu - otworz)
+        perror("blad ustawienia semafora 2");
         exit(EXIT_FAILURE);
     }
-    if (semctl(id_sem, 3, SETVAL, 1) == -1) { //semafor ochroniarz 2 (blokada sklepu)
-        perror("blad ustawienia semafora 3");
-        exit(EXIT_FAILURE);
-    }
-    sem_p(3);
-    FILE *file = fopen("raport_dzienny.txt", "w"); //plik raport (nadpisz)
+    sem_p(1); //wyczysc ksiege gosci
+    FILE *file = fopen("ksiega_gosci.txt", "w"); //plik ksiega_gosci (wyczysc)
     if (file == NULL) {
         perror("problem otwarcia pliku");
         exit(EXIT_FAILURE);
     }
-    fprintf(file, "%s", "---------RAPORT-DZIENNY--------\n"); //zawartosc do pliku
+    //fprintf(file, "%s", "---------RAPORT-DZIENNY--------\n"); //zawartosc do pliku
     fclose(file);
-    sem_v(3);
+    sem_v(1); //otworz blokade 
     printf("\tSklep otwarty\n");
 }
 
-//zamykanie nocne
+//zamykanie nocne - zamknij sklep, policz klientow z dnia
 void zamknij_sklep(void) {
-    sem_p(3); //ochroniarz pilnuje wejscia, by nikt nie wszedl
+    printf("\tZamknij sklep\n");
+    sem_p(2); //ochroniarz pilnuje wejscia, by nikt nie wszedl
     while(1) {
         sleep(1);
-        if(semctl(id_sem, 0, GETVAL) == 30) { //wszyscy klienci wyszli
-            printf("Nikogo nie ma na w sklepie\n");
-            semctl(id_sem, 0, SETVAL, 0);
+        if(semctl(id_sem, 0, GETVAL) == 30) { //czy wszyscy klienci wyszli
+            printf("\nNikogo nie ma w sklepie\n");
+            //semctl(id_sem, 0, SETVAL, 0);
             printf("Nikogo nie ma na kasach\n");
             break;
         }
     }
-    //dopisz do raport miesieczny...
 
-    printf("\tSklep zamkniety\n");
+    //dopisz do raport miesieczny...
+    printf("przeliczanie liczby klientow... \n");
+
+    //przelicz klientow
+    int potok1[2]; // cat ksiega_gosci.txt |wc -c 
+ 	if (pipe(potok1) == -1){
+ 		perror("pipe error"); 
+ 		exit(EXIT_FAILURE);
+ 	}
+
+    pid_t a = fork();
+ 	if(a == -1) { 
+ 		perror("fork error");
+ 		exit(EXIT_FAILURE);
+    } else if (a == 0) {
+ 		close(potok1[0]);
+ 		dup2(potok1[1], 1);
+        //sleep(1);
+ 		execlp("cat","cat", "ksiega_gosci.txt",NULL);
+		perror("execlp 'cat' error");
+ 		exit(EXIT_FAILURE);
+    } else {
+	    close(potok1[1]);
+ 	    dup2(potok1[0], 0);
+        close(potok1[0]);
+        sleep(1);
+            //printf("\n\tLiczba klientow: ");
+ 		execlp("wc", "wc", "-c", NULL);
+ 		perror("execlp 'wc' error");
+		exit(EXIT_FAILURE);
+    }
+ 			
 }
 
+//usun semfory
 void zakoncz_program() {
+    printf("\n\t KONCZE PROGRAM \n");
     if (semctl(id_sem, 0, IPC_RMID) == -1) {
-        perror("nie mozna usunoac semafora");
+        perror("blad usunania semaforow");
         exit(EXIT_FAILURE);
     }
-    if (fork() == 0) {
+    if (msgctl(id_kolejki, IPC_RMID, NULL) == -1) {
+        perror("blad usuniecia kolejki");
+        exit(EXIT_FAILURE);
+    }
+    /*if (fork() == 0) {
         execlp("cat", "raport_dzienny.txt", NULL);
         perror("nie udany execlp");
         exit(EXIT_FAILURE);
-    }
+    }*/
 }
 
+void *obsluga_kasy(void* arg) {
+    int msgid = *(int*)arg;
+    struct msg_buffer message;
+    printf("\tWatek %d jest na kasie\n", getpid());
+    while (1) {
+        // Odczytanie komunikatu
+        //printf("\tKierownik odczytał komunikat: %s\n", message.mtext);
+        if (msgrcv(msgid, &message, sizeof(message.mtext), 0, 0) == -1) {
+            perror("blad odczytania komuinkatu");
+            //exit(EXIT_FAILURE);
+            continue;
+        }
+
+        printf("\tKierownik odczytał komunikat: %s\n", message.mtext);
+
+        // Usuwanie komunikatu po odczycie jest automatyczne w System V
+        sleep(1);  // Symulacja czasu przetwarzania
+    }
+
+    return NULL;
+
+}
 
 int main(){
-    id_sem = semget(klucz, 4, 0600 | IPC_CREAT); //semaforki
+    id_sem = semget(klucz, 3, 0600 | IPC_CREAT); //semaforki
     if (id_sem == -1) {
         perror("blad tworzenia semfarow");
         exit(EXIT_FAILURE);
     }
+    id_kolejki = msgget(klucz, 0600 | IPC_CREAT);
+    if(id_kolejki == -1) {
+		printf("Blad tworzenia kolejki komunikatow.\n");
+		exit(EXIT_FAILURE);
+	}
 
     otworz_sklep();
     int ilosc_klientow = 30 - semctl(id_sem, 0, GETVAL);
     printf("Ilosc klientow: %d\n", ilosc_klientow);
 
+    // Tworzenie wątku do obsługi kolejki
+    pthread_t thread_id;
+    if (pthread_create(&thread_id, NULL, obsluga_kasy, &id_kolejki) != 0) {
+        perror("pthread_create");
+        exit(EXIT_FAILURE);
+    }
+    // Oczekiwanie na zakończenie wątku (teoretycznie nieskończone)
+    pthread_join(thread_id, NULL);
 
 
-    sleep(3);
+    //printf("\n\t2.00=%d\n", semctl(id_sem, 2, GETVAL));
+    otworz_sklep();
+    ilosc_klientow = 30 - semctl(id_sem, 0, GETVAL);
+    printf("Ilosc klientow: %d\n", ilosc_klientow);
+    //printf("\n\t2.01=%d\n", semctl(id_sem, 2, GETVAL));
+    //sem_p(2);
+    //printf("\t2=%d\n", semctl(id_sem, 2, GETVAL));
+
+
+
+
+
+    sleep(5);
+    ilosc_klientow = 30 - semctl(id_sem, 0, GETVAL);
+    printf("Ilosc klientow: %d\n", ilosc_klientow);
+    //printf("\n\t2.02=%d\n", semctl(id_sem, 2, GETVAL));
+
     zamknij_sklep();
     zakoncz_program();
 
