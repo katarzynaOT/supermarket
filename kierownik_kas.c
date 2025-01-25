fb51a1067fea30bd4a37fe012004a75148cbfde7
KasiaOT <167691567+katarzynaOT@users.noreply.github.com>
2025-01-25 22:31:40 +0100

    poprawne odbieranie sygnalow v1

--- kierownik_kas.c
+++ kierownik_kas.c
@@ -25,15 +25,18 @@
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_WHITE   "\x1B[37m"
 
 #define MAX_TEXT 20 //ilosc znakow do zapisu do kolejki
-#define DZIEN 30 //czas trwania jednego dnia
+#define DZIEN 100 //czas trwania jednego dnia
 #define MAX_KASY 10 //maksymalna liczba aktywnych kas
 #define KLIENCI_NA_KASE 5 //liczba klientow na kase - zmienna
 
 int N; //liczba czynnych kas
 int K; //liczba klientow w sklepie
 key_t klucz = 21370; //ftok("pp", 127);
+
+volatile sig_atomic_t pozar = 0; //do powiadomienia sklepu o pozarze
 
 int id_sem; 
 int id_kolejki;
@@ -45,7 +48,6 @@ struct dane_kasy {
     int id_msg;
     int id_nowej_kasy;
 };
-
 
 static void sem_p(int nr) { //opuszczenie semafora
     struct sembuf bufor_sem;
@@ -84,7 +86,7 @@ void pisz_raport(const char *tresc) {
 
 //otwieranie po nocy - wlacz semafory, wyczysc ksiege gosciS
 void otworz_sklep(void) {
-    if (semctl(id_sem, 0, SETVAL, 30) == -1) { //semafor ochroniarz 1 (odpowiada za to, by sklep sie nie przepelnil)
+    if (semctl(id_sem, 0, SETVAL, 30) == -1) { //semafor ochroniarz 1 - ilosc klientow (odpowiada za to, by sklep sie nie przepelnil)
         perror("blad ustawienia semafora 0");
         exit(EXIT_FAILURE);
     }
@@ -96,7 +98,7 @@ void otworz_sklep(void) {
         perror("blad ustawienia semafora 2");
         exit(EXIT_FAILURE);
     }
-    if (semctl(id_sem, 3, SETVAL, 2) == -1) { // ilosc aktywnych kas (na poczatku 2)
+    if (semctl(id_sem, 3, SETVAL, 2) == -1) { //ilosc aktywnych kas (na poczatku 2)
         perror("blad ustawienia semafora 3");
         exit(EXIT_FAILURE);
     }
@@ -114,14 +116,14 @@ void otworz_sklep(void) {
 
 //zamykanie nocne - zamknij sklep, policz klientow z dnia
 void zamknij_sklep(void) {
-    printf("\tZamknij sklep\n");
+    printf("\tDecyzja o zamknieciu sklepu\n");
     sem_p(2); //ochroniarz pilnuje wejscia, by nikt nie wszedl
-    while(1) {
+    while(1) { //czekam na obsluge pozostalych klietnow
         sleep(1);
         if(semctl(id_sem, 0, GETVAL) == 30) { //czy wszyscy klienci wyszli
             printf("\nNikogo nie ma w sklepie\n");
             //semctl(id_sem, 0, SETVAL, 0);
-            printf("Nikogo nie ma na kasach\n");
+            //printf("Nikogo nie ma na kasach\n");
             break;
         }
     }
@@ -148,21 +150,27 @@ void zamknij_sklep(void) {
 		perror("execlp 'cat' error");
  		exit(EXIT_FAILURE);
     } else {
-	    close(potok1[1]);
- 	    dup2(potok1[0], 0);
-        close(potok1[0]);
-        sleep(1);
-            //printf("\n\tLiczba klientow: ");
- 		execlp("wc", "wc", "-c", NULL);
- 		perror("execlp 'wc' error");
-		exit(EXIT_FAILURE);
+        pid_t b = fork();
+        if(b == -1) {
+            perror("fork error");
+            exit(EXIT_FAILURE);
+        } else if (b == 0) {
+            close(potok1[1]);
+ 	        dup2(potok1[0], 0);
+            close(potok1[0]);
+            sleep(1);
+ 		    execlp("wc", "wc", "-w", NULL);
+ 	    	perror("execlp 'wc' error");
+		    exit(EXIT_FAILURE);
+        }
     }
  			
 }
 
 //usun semfory i kolejki
 void zakoncz_program() {
-    printf("\n\t KONCZE PROGRAM \n");
+    //sleep(1);
+    printf("\n\t KONCZE PROGRAM... %d\n", getpid());
     if (semctl(id_sem, 0, IPC_RMID) == -1) {
         perror("blad usunania semaforow");
         exit(EXIT_FAILURE);
@@ -176,28 +184,66 @@ void zakoncz_program() {
         perror("nie udany execlp");
         exit(EXIT_FAILURE);
     }*/
+    printf("\t\tKONIEC\n");
+    exit(0);
 }
+
+//pozar
+void pozar_alarm(int sig) {
+    if (sig == SIGUSR1) {
+        pozar = 1;
+        printf("\tPracownik %d sklepu wie, ze jest pozar\n", getpid());
+        sem_p(3); //opusc swoje stanowisko kasowwe
+        while(1) { //czekam na ewakuacje klientow
+            sleep(1);
+            //printf("Pracownik %d czeka na wyjscie klietnow\n", getpid());
+            if(semctl(id_sem, 0, GETVAL) == 30) { //czy wszyscy klienci wyszli
+                printf("\nNikogo nie ma w sklepie -> zamknij kasy\n");
+                semctl(id_sem, 0, SETVAL, 0);
+                //printf("Nikogo nie ma na kasach\n");
+                break;
+            }
+        }
+        printf("\tPracownik (z kasy) %d ucieka ze sklepu.\n", getpid());
+        zakoncz_program();
+        exit(0); //uciekaj jak nie ma juz klientow w sklepie
+    }
+
+}
+
+
+//komentarze zostac i w raporcie je rozszerzyc jeszzce
+//min 4 testy
+//interfejs, github, kod podzielony na procedury
+//powinno dzialac bez sleep
+//obsluga bledu - perror
+//obsluga wyjatkow - przechwycenie bledu-wyjatku i robienie cso z nim dalej
+
 
 void *obsluga_kasy(void* arg) {
     struct dane_kasy* dane = (struct dane_kasy*)arg; // Rzutowanie wskaźnika void* na struct dane_kasy*
-    int msg_id = dane->id_msg;
+    int msg_id = dane->id_msg; //id kolejki
     int id_kasy = dane->id_nowej_kasy;
 
     struct msg_buffer message;
-    printf("\tWatek %ld jest na kasie %d", pthread_self(), id_kasy);
+//int liczba_klientow = 30 - semctl(id_sem, 0, GETVAL);
+//int liczba_kas = semctl(id_sem, 3, GETVAL);
+//int wzor = (liczba_kas - 1) * KLIENCI_NA_KASE;
+//printf(ANSI_COLOR_WHITE "\t\tIV. kl = %d, ka = %d, wz = %d\n", liczba_klientow, liczba_kas, wzor);
+    printf(ANSI_COLOR_WHITE "\tWatek %ld jest na kasie %d\n", pthread_self(), id_kasy);
+    sleep(2);
 
-    while ( semctl(id_sem, 3, GETVAL) == id_kasy ) { //dopoki kasa otwarta i ktos czeka w kolejce
+    while ( semctl(id_sem, 3, GETVAL) <= id_kasy && !pozar) { //dopoki kasa otwarta i nie ma pozaru (i ktos czeka w kolejce - to do)
 //obsluga kllienta na kasie
         if (msgrcv(msg_id, &message, sizeof(message.mtext) - sizeof(long), id_kasy, 0) == -1) {
             perror("blad odczytania komuinkatu");
             exit(EXIT_FAILURE);
-            //continue;
         }
         printf(ANSI_COLOR_MAGENTA "Kasa %d obsluguje klienta %s\n" ANSI_COLOR_RESET, id_kasy, message.mtext);       
         sleep((rand() % 11) + 1); ///czas obslugi jednego klienta (od 1 do 11)
+        printf("d2 = %d\n", pozar);
     }
     return NULL;
-
 }
 
 int main(){
@@ -212,70 +258,95 @@ int main(){
 		exit(EXIT_FAILURE);
 	}
 
+    struct sigaction sa; //sygnal
+    sa.sa_handler = pozar_alarm;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("blad ustawienia sygnalu");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (fork() == 0) {
+        sleep(DZIEN);
+        zamknij_sklep();
+        zakoncz_program();
+        exit(0);
+    }
+
+    pthread_t kasy[MAX_KASY]; //pracownicy, ktorzy moga pracowac na kasach - istnieja, ale nie wszyscy obsluguja kase (niektorzy maja przerwy czy cos)
+
     otworz_sklep();
     int ilosc_klientow = 30 - semctl(id_sem, 0, GETVAL);
     printf("Ilosc klientow: %d\n", ilosc_klientow);
+    printf("Mamy %d otwartych kas\n", semctl(id_sem, 3, GETVAL));
+
+    for (int i = 0; i < 2; i++) { //otworzenie dwoch poczatkowych kas, przy otwarciu sklepu
+        struct dane_kasy* dane = malloc(sizeof(struct dane_kasy));
+        if (dane == NULL) {
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
+        dane->id_msg = id_kolejki;
+        dane->id_nowej_kasy = i;
+//int liczba_klientow = 30 - semctl(id_sem, 0, GETVAL);
+//int liczba_kas = semctl(id_sem, 3, GETVAL);
+//int wzor = (liczba_kas - 1) * KLIENCI_NA_KASE;
+//printf(ANSI_COLOR_WHITE "\t\tII. kl = %d, ka = %d, wz = %d\n", liczba_klientow, liczba_kas, wzor);
+        if (pthread_create(&kasy[i], NULL, obsluga_kasy, dane) != 0) { //pracownik idze na nowa kase (0,1)
+            perror("pthread_create");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    sleep(2);
 
 
-
-
-    pthread_t kasy[MAX_KASY]; //pracownicy, ktorzy moga pracowac na kasach - istnieja, ale nie wszyscy obsluguja kase (niektorzy maja przerwy czy cos)
-//ile kas potrzebujemy
-    while (1) {
-        int liczba_klientow = 30 - semctl(id_sem, 1, GETVAL);
+//ile kas potrzebujemy - sprawdzamy dopoki sklep otwarty 
+    while (semctl(id_sem, 2, GETVAL) != 0 && !pozar) { //dopoki sklep otwarty && nie ma pozaru
+        int liczba_klientow = 30 - semctl(id_sem, 0, GETVAL);
         int liczba_kas = semctl(id_sem, 3, GETVAL);
-        if ( liczba_klientow >= ((liczba_kas - 1) * KLIENCI_NA_KASE) && liczba_kas < 10 ) { //zwiekszenie liczby kas
+        int wzor = (liczba_kas - 1) * KLIENCI_NA_KASE;
+//printf(ANSI_COLOR_WHITE "\t\tI. kl = %d, ka = %d, wz = %d\n", liczba_klientow, liczba_kas, wzor);
+//zwiekszenie liczby kas
+        if ( (liczba_klientow > wzor) && (liczba_kas < 10) ) { 
+//printf(ANSI_COLOR_WHITE "\tZwieksz kasy, pp = %d, p = %d\n", (liczba_kas - 1) * KLIENCI_NA_KASE, (liczba_klientow > ((liczba_kas - 1) * KLIENCI_NA_KASE)));
+sleep(1);
             struct dane_kasy* dane = malloc(sizeof(struct dane_kasy));
             if (dane == NULL) {
                 perror("malloc");
                 exit(EXIT_FAILURE);
             }
             dane->id_msg = id_kolejki;
-            dane->id_nowej_kasy = liczba_kas + 1;
-            if (pthread_create(&kasy[liczba_kas+1], NULL, obsluga_kasy, dane) != 0) { //pracownik idze na nowa kase
+            dane->id_nowej_kasy = liczba_kas;
+//printf(ANSI_COLOR_WHITE "\t\tIII. kl = %d, ka = %d, wz = %d\n", liczba_klientow, liczba_kas, wzor);
+            if (pthread_create(&kasy[liczba_kas], NULL, obsluga_kasy, dane) != 0) { //pracownik idze na nowa kase
                 perror("pthread_create");
                 exit(EXIT_FAILURE);
             }
             sem_v(3); //otworz nowa kase
-        } else if ( liczba_klientow < ((liczba_kas - 1) * KLIENCI_NA_KASE) && liczba_kas > 2 ) { //zmniejszenie liczby kas
+//zmniejszenie liczby kas
+        } else if ( (liczba_klientow < wzor) && (liczba_kas > 2) ) { 
+//printf(ANSI_COLOR_WHITE "\tZmniejsz kasy, pp = %d, p = %d\n", (liczba_kas - 1) * KLIENCI_NA_KASE, (liczba_klientow > ((liczba_kas - 1) * KLIENCI_NA_KASE)));
+sleep(1);            
             sem_p(3); //zamykamy kase
             pthread_t pracownik;
 
         }
-
+        sleep(1);
     }
 
-
-
-
-    // Tworzenie wątków dla kas
-    pthread_t kasy[MAX_KASY];
-    for (int i = 0; i < MAX_KASY; i++) {
-        if (pthread_create(&kasy[i], NULL, obsluga_kasy, &id_kolejki) != 0) {
-            perror("pthread_create");
-            exit(EXIT_FAILURE);
-        }
-    }
-    // Czekanie na zakończenie wątków (teoretycznie nieskończone)
-    for (int i = 0; i < MAX_KASY; i++) {
+//obslugiwanie kas po zamknieciu sklepu - czekanie na zakonczenie watkow
+    int liczba_kas = semctl(id_sem, 3, GETVAL);
+    for (int i = 0; i < liczba_kas; i++) {
         pthread_join(kasy[i], NULL);
     }
-
-
-
-
-
-    otworz_sklep();
+    
     ilosc_klientow = 30 - semctl(id_sem, 0, GETVAL);
     printf("Ilosc klientow: %d\n", ilosc_klientow);
 
-
-    //sleep(5);
-    ilosc_klientow = 30 - semctl(id_sem, 0, GETVAL);
-    printf("Ilosc klientow: %d\n", ilosc_klientow);
-
-    zamknij_sklep();
-    zakoncz_program();
+    //czy potrzebne?
+    while (wait(NULL) > 0); //rodzic czeka na ukonczenie wszystkich potomkow
 
     return 0;
 }
